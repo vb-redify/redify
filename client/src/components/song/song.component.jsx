6b000d7492356b805771aae8a55ca528a915bef3
Brian Bouchard <b.bouch@gmail.com>
2020-06-23 15:49:42 -0600

    Set up skeleton for playlist liked songs list

--- client/src/components/song/song.component.jsx
+++ client/src/components/song/song.component.jsx
@@ -3,8 +3,15 @@ import PropTypes from 'prop-types';
 import './song.styles.css';
 
 const Song = ({ song }) => (
-  <div className="song-container">
-    {song.title}
+  <div className='song-container'>
+    <div>Play</div>
+    <div className='song-details'>
+      <div>{song.title}</div>
+      <div>
+        {song.explicit} - {song.artist} - {song.album}
+      </div>
+    </div>
+    <div>{song.minutes}:{song.seconds}</div>
   </div>
 );
 
