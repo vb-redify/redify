fd2ff82abe20ef3670fc45e89ef659dda2145871
Brian Bouchard <b.bouch@gmail.com>
2020-06-24 08:20:37 -0600

    Swap song icon on hover

--- client/src/components/song/song.component.jsx
+++ client/src/components/song/song.component.jsx
@@ -1,26 +1,51 @@
-import React from 'react';
+import * as React from 'react';
 import PropTypes from 'prop-types';
 import './song.styles.css';
 
-const Song = ({ song }) => (
-  <div className="song-container">
-    <div className="song-play">
-      <img src="./images/music.svg" alt="Play" />
-    </div>
-    <div className="song-details">
-      <div>{song.title}</div>
-      <div className="song-artist-album">
-        {song.explicit}
-        {song.artist}
-        <span> &middot; </span>
-        {song.album}
+  // const [songImage, setSongImage] = useState('music');
+
+  // return (
+    // <div
+      // className="song-container"
+      // onMouseOver={() => setSongImage('play')}
+      // onFocus={() => setSongImage('play')}
+      // onMouseOut={() => setSongImage('music')}
+      // onBlur={() => setSongImage('music')}
+    // >
+      // <div className="song-play">
+        {/* <img src={`./images/${songImage}.svg`} alt="Play" /> */}
+      {/* </div> */}
+
+const Song = ({ song }) => {
+  const [songImage, setSongImage] = React.useState('music');
+  return (
+    <div
+      className="song-container"
+      onMouseOver={() => setSongImage('play2')}
+      onFocus={() => setSongImage('play2')}
+      onMouseOut={() => setSongImage('music')}
+      onBlur={() => setSongImage('music')}
+    >
+      <div className="song-play">
+        <img src={`./images/${songImage}.svg`} alt={`${songImage}`} />
+      </div>
+      <div className="song-details">
+        <div>{song.title}</div>
+        <div className="song-artist-album">
+          {song.explicit}
+          {song.artist}
+          <span> &middot; </span>
+          {song.album}
+        </div>
+      </div>
+      <div>
+        {song.minutes}
+        :
+        {song.seconds}
       </div>
     </div>
-    <div>
-      {song.minutes}:{song.seconds}
-    </div>
-  </div>
-);
+  );
+};
 
 Song.propTypes = {
   song: PropTypes.shape({
