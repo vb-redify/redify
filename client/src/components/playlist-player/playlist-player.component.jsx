f56caf73641e7afcd88bccbefaa5c2b66ff8ffd2
Brian Bouchard <b.bouch@gmail.com>
2020-06-25 12:17:25 -0600

    Add play image to playlist

---
+++ client/src/components/playlist-player/playlist-player.component.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './playlist-player.styles.css';
+
+const PlaylistPlayer = () => (
+  <div>
+    <img src="./images/play2.svg" alt="Play Playlist" />
+  </div>
+);
+
+export default PlaylistPlayer;
--- client/src/pages/liked-songs/liked-songs.component.jsx
+++ client/src/pages/liked-songs/liked-songs.component.jsx
@@ -2,11 +2,13 @@ import * as React from 'react';
 
 import PlaylistHeader from '../../components/playlist-header/playlist-header.component';
 import PlaylistList from '../../components/playlist-list/playlist-list.component';
+import PlaylistPlayer from '../../components/playlist-player/playlist-player.component';
 import './liked-songs.styles.css';
 
 const LikedSongsPage = () => (
   <div className="liked-songs-container">
     <PlaylistHeader>Liked Songs</PlaylistHeader>
+    <PlaylistPlayer />
     <PlaylistList />
   </div>
 );
