ec6e7384fa7075c13986730f35335cb723499b92
Brian Bouchard <55816957+bee-squared@users.noreply.github.com>
2020-06-30 14:41:06 -0600

    Jest/Enzyme Initialization and Setup (#22)
    * Initialize Jess and Set Up A Basic Test
    
    * Add jest config file
    
    * Use Enzyme and Add a Playlist Header Unit Test

--- client/src/App.test.jsx
+++ client/src/App.test.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { shallow } from 'enzyme';
 import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('App', () => {
+  it('should render a <div />', () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.find('div').length).toEqual(1);
+  });
 });
